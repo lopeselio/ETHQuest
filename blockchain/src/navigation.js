ea5380bfdc7657bbc31d5742808b33cfd543a1fe
devELIOper <lopeselio@gmail.com>
2022-07-24 10:35:07 +0530

    navigation and routing files

---
+++ blockchain/src/navigation.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import {BrowserRouter, Switch, Route } from 'react-router-dom';
+import history from './history';
+
+import Selection from './components/Selection';
+import Upload from './components/Upload';
+import Login from './components/Login';
+import HeroBooth from './components/HeroBooth';
+import StoryBooth from './components/StoryBooth';
+import App from './components/App';
+import Landing from './components/Landing'
+import { UserContext } from './utils/UserContext'
+
+
+
+export default function Routes () {
+    const [user, setUser] = useState(undefined)
+        return (
+            <BrowserRouter history={history}>
+                <UserContext.Provider value={{user, setUser}}>
+
+                <Switch>
+                    {/* <Route path="/" exact component={Landing} /> */}
+                    <Route path="/" exact component={Selection} />
+
+                    <Route path="/Upload" exact component={Upload} />
+                    <Route path="/HeroUpload" exact component={Upload} />
+                    <Route path="/StoryUpload" exact component={Upload} />
+                    <Route path="/Login" exact component={Login} />
+                    <Route path="/HeroBooth" exact component={HeroBooth} />
+                    <Route path="/StoryBooth" exact component={StoryBooth} />
+                    <Route path="/App" exact component={App} />
+
+                </Switch>
+        </UserContext.Provider>
+            </BrowserRouter>
+        )
+    
+}
